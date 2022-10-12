2158b57b213e5ae24545b1c9ea3af88fd93ad19d
Mikoto Misaka <1355016743@qq.com>
2022-10-12 16:31:53 +0800

    界面槽函数初步绑定

--- main.py
+++ main.py
@@ -17,15 +17,24 @@ class DetectCore(QObject):
     def __init__(self):
         super(DetectCore, self).__init__()
 
-    def getfile(self):
-        name = QFileDialog.getOpenFileName()
-        if len(name[0]):
-            return name[0]
+    def imgdetect(self):  # 将来增加模型选择功能
+        name = QFileDialog.getOpenFileName(caption='选择要识别的图片', filter='Images (*.bmp *.dng, *.jpeg *.jpg *.mpo *.png '
+                                                                      '*.tif *.tiff *.webp')
+        if len(name[0]):    # 记得改权重为训练后的新权重
+            detect.run(weights='yolov5s.pt', source=name[0])
         else:
             pass
 
-    def imgdetect(self):  # 将来增加模型选择功能
-        detect.run(weights='yolov5s.pt', source=self.getfile())
+    def viddetect(self):
+        name = QFileDialog.getOpenFileName(caption='选择要识别的视频', filter='Videos (*.asf *.avi *.gif *.m4v *.mkv *.mov '
+                                                                      '*.mp4 *.mpeg *.mpg *.ts *.wmv')
+        if len(name[0]):
+            detect.run(weights='yolov5s.pt', source=name[0])
+        else:
+            pass
+
+    def camdetect(self):
+        pass
 
 
 class UI_init(QObject):
@@ -34,25 +43,42 @@ class UI_init(QObject):
     def __init__(self):
         super(UI_init, self).__init__()
         # 绑定槽函数
+        self.initui1()
+
+    # 把UI 的返回键去掉，不做返回功能了
+    def initui1(self):
         ui1.imgDetectButton.clicked.connect(self.initui2)
+        ui1.videoDetectButton.clicked.connect(self.initui3)
+        ui1.cameraDetectButton.clicked.connect(self.initui4)
 
     def initui2(self):
-        Mainwindow2 = QMainWindow()
         ui2 = UI.ui_2.Ui_MainWindow()
-        ui2.setupUi(Mainwindow2)
-        Mainwindow2.show()
+        ui2.setupUi(Mainwindow)
+        Mainwindow.show()
+        ui2.selectImageButton.clicked.connect(core.imgdetect)
 
+    def initui3(self):
+        ui3 = UI.ui_3.Ui_MainWindow()
+        ui3.setupUi(Mainwindow)
+        Mainwindow.show()
+        ui3.selectVideoButton.clicked.connect(core.viddetect)
 
+    def initui4(self):
+        ui4 = UI.ui_4.Ui_MainWindow()
+        ui4.setupUi(Mainwindow)
+        Mainwindow.show()
+        ui4.selectCameraButton.clicked.connect(core.camdetect)
 
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    Mainwindow1 = QMainWindow()
+    Mainwindow = QMainWindow()
 
     ui1 = UI.ui_1.Ui_MainWindow()
-    ui1.setupUi(Mainwindow1)
+    ui1.setupUi(Mainwindow)
 
-    UI_init()
-    Mainwindow1.show()
+    ui = UI_init()   # 此处实例化UI_init必须要将实例赋值给一个变量，否则构造函数初始化的信号连接将会被回收/中断
+    core = DetectCore()
+    Mainwindow.show()
     sys.exit(app.exec())
